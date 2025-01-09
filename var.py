b506682e6fa29480712cd0993b80ed3e18ae79bf
VoidOG <VoidGod123@outlook.com>
2025-01-09 18:25:31 +0530

    Update var.py

--- var.py
+++ var.py
@@ -14,8 +14,8 @@ def make_int(str_input):
 class Var:
     API_ID = int(os.getenv("API_ID", "29872536"))
     API_HASH = os.getenv("API_HASH", "65e1f714a47c0879734553dc460e98d6")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7996930077:AAFMfiyoygYldEYNOb1J8GdVU-yjeGB_5B0")
-    sudo = os.getenv("SUDO","6551446148 6663845789 1110013191 7163357187")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7382235042:AAFv5nrAHJEnq3cuJUOTCGLKYdVDeIaYZnE")
+    sudo = os.getenv("SUDO","6551446148 6663845789 1110013191 7163357187 7253322919")
     SUDO = []
     if sudo:
         SUDO = make_int(sudo)
